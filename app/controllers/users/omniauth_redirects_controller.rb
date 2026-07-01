e397ee3dcd20eac2fbb728dc09b2592f5cbdc0f3
青木 <aoki@aokinoMacBook-Air.local>
2026-07-01 23:15:59 +0900

    Googleログインのエラー修正

--- app/controllers/users/omniauth_redirects_controller.rb
+++ app/controllers/users/omniauth_redirects_controller.rb
@@ -3,6 +3,7 @@ module Users
     skip_before_action :authenticate_user!, only: :google_oauth2
 
     def google_oauth2
+      redirect_to user_google_oauth2_omniauth_authorize_path, allow_other_host: true, status: :see_other
     end
   end
 end
